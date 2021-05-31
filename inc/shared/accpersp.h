0dc9eac2afd39929cb28e187634e81945f7724d8
Takashi Kanai <kanait@acm.org>
2021-06-01 03:16:25 +0900

    "inc" directory deleted.

--- inc/shared/accpersp.h
+++
@@ -1,8 +0,0 @@
-void accFrustum(GLdouble left, GLdouble right, GLdouble bottom, 
-                GLdouble top, GLdouble nnear, GLdouble ffar, GLdouble pixdx, 
-                GLdouble pixdy, GLdouble eyedx, GLdouble eyedy, GLdouble focus);
-
-void accPerspective(GLdouble fovy, GLdouble aspect, 
-                    GLdouble nnear, GLdouble ffar, GLdouble pixdx, GLdouble pixdy, 
-                    GLdouble eyedx, GLdouble eyedy, GLdouble focus);
-
